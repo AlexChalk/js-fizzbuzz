6c4dfa530f6dddfa460d2bc89b2fac04d689fbc4
SimonTanner <simonthetanner@hotmail.com>
2017-05-08 17:45:48 +0100

    Passes test for 15 to output 'buzz'

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,25 +1,34 @@
 describe('FizzBuzz', function () {
-    var fizzBuzz;
+  var fizzBuzz;
 
-    beforeEach(function() {
+  beforeEach(function() {
 	fizzBuzz = new FizzBuzz();
     });
 
-    describe('play', function () {
-	it('Returns 1 when passed 1', function () {
-	    expect(fizzBuzz.play(1)).toEqual(1); 
-	});
+  describe('play', function () {
+  	it('Returns 1 when passed 1', function () {
+  	    expect(fizzBuzz.play(1)).toEqual(1);
+  	});
 
-	it('Returns 2 when passed 2', function() {
-	    expect(fizzBuzz.play(2)).toEqual(2);
-	});
+  	it('Returns 2 when passed 2', function() {
+  	    expect(fizzBuzz.play(2)).toEqual(2);
+  	});
 
-	it('Returns "fizz" when passed 3', function() {
-	    expect(fizzBuzz.play(3)).toEqual("fizz");
-	});
+  	it('Returns "fizz" when passed 3', function() {
+  	    expect(fizzBuzz.play(3)).toEqual("fizz");
+  	});
 
-	it('Returns 4 when passed 4', function() {
-	    expect(fizzBuzz.play(4)).toEqual(4);
-	});
-    });
+  	it('Returns 4 when passed 4', function() {
+  	    expect(fizzBuzz.play(4)).toEqual(4);
+  	});
+
+    it('Returns "buzz" when passed 5', function() {
+  	    expect(fizzBuzz.play(5)).toEqual("buzz");
+  	});
+
+    it('Returns "fizzbuzz" when passed 15', function() {
+  	    expect(fizzBuzz.play(15)).toEqual("fizzbuzz");
+  	});
+    
+  });
 });
